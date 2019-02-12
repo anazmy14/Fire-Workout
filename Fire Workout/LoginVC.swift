dafdf12fa9968ad38d234a9d15f87de5fc975342
an <an@ans-MacBook-Pro.local>
2019-02-12 21:09:50 +0200

    move text field when keyboard show

--- Fire Workout/LoginVC.swift
+++ Fire Workout/LoginVC.swift
@@ -9,10 +9,11 @@
 import UIKit
 import FirebaseAuth
 
-class LoginVC: UIViewController {
+class LoginVC: UIViewController , UITextFieldDelegate  {
 
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginBtn: UIStackView!
     @IBAction func login(_ sender: Any) {
         if email.text != "" || password.text != ""  {
             Auth.auth().signIn(withEmail: email.text! , password: password.text!) { (result , error ) in
@@ -27,12 +28,33 @@ class LoginVC: UIViewController {
         }
     }
     
-    
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:) ) , name: UIResponder.keyboardWillShowNotification , object: nil )
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:) ) , name: UIResponder.keyboardWillHideNotification , object: nil )
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:) ) , name: UIResponder.keyboardWillChangeFrameNotification , object: nil )
+    }
+    
+    deinit {
+      NotificationCenter.default.removeObserver(self , name: UIResponder.keyboardWillShowNotification, object: nil)
+      NotificationCenter.default.removeObserver(self , name: UIResponder.keyboardWillHideNotification, object: nil)
+       NotificationCenter.default.removeObserver(self , name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+
+    }
+    
+    
+    @objc func keyboardWillChange (notification : Notification ) {
+        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        
+        self.view.frame.origin.y = -(keyboardRect.height - (view.frame.height - ( loginBtn.frame.origin.y + loginBtn.frame.height )  )  )
+        print(notification.name)
     }
     
 
--- Fire Workout/RegisterVC.swift
+++ Fire Workout/RegisterVC.swift
@@ -14,6 +14,7 @@ class RegisterVC: UIViewController {
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var confirmPassword: UITextField!
+    @IBOutlet weak var registerBtn: ovalButton!
     @IBAction func register(_ sender: Any) {
         
         if password.text != confirmPassword.text {
@@ -36,24 +37,39 @@ class RegisterVC: UIViewController {
         }
     }
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:) ) , name: UIResponder.keyboardWillShowNotification , object: nil )
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:) ) , name: UIResponder.keyboardWillHideNotification , object: nil )
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:) ) , name: UIResponder.keyboardWillChangeFrameNotification , object: nil )
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    deinit {
+        NotificationCenter.default.removeObserver(self , name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self , name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self , name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
     }
-    */
-
+    
+    @objc func keyboardWillChange (notification : Notification ) {
+        guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        
+        self.view.frame.origin.y = -(keyboardRect.height - (view.frame.height - ( registerBtn.frame.origin.y + registerBtn.frame.height )  )  )
+        print(notification.name)
+    }
+    
 }
+
+
+
+
+
+
 
 func displayError ( message:String , controller:UIViewController )  {
     let alert = UIAlertController(title: "ERORR ", message: message , preferredStyle: .alert )
