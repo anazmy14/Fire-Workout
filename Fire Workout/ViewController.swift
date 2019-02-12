a5ce20fbffd8a64998e47509c7180c7a97be5ad7
an <an@ans-MacBook-Pro.local>
2019-02-12 17:10:15 +0200

    Initial Commit

---
+++ Fire Workout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fire Workout
+//
+//  Created by an on 2/12/19.
+//  Copyright © 2019 an. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
