000cfa2a85cffcbe6066f02aa4420b8df19cbac2
an <an@ans-MacBook-Pro.local>
2019-02-16 01:28:27 +0200

    Plan levels

---
+++ Fire Workout/Model/PlanLevel.swift
@@ -0,0 +1,16 @@
+//
+//  planLevel.swift
+//  Fire Workout
+//
+//  Created by an on 2/15/19.
+//  Copyright © 2019 an. All rights reserved.
+//
+
+import Foundation
+
+enum PlanLevel:Int {
+    case beginner = 0
+    case intermediate
+    case advanced
+}
+    
